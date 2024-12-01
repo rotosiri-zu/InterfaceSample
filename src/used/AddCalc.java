8ce4fd8b70a1bd7833b24d09e838639d0f3e62da
rotosiri-zu <hongjitufang25@gmail.com>
2024-12-01 11:40:11 +0900

    Callクラスの追加、Add,Subクラスのコード修正

--- src/used/AddCalc.java
+++ src/used/AddCalc.java
@@ -1,11 +1,14 @@
 package used;
 
+/**
+*Calculator実装クラス<br>
+*加算を行う
+*/
 public class AddCalc implements Calculator {
 
 	@Override
 	public Integer calc(Integer x, Integer y) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return x + y;
 	}
 
 }
--- src/used/SubCalc.java
+++ src/used/SubCalc.java
@@ -1,11 +1,14 @@
 package used;
 
+/**
+ * Calculator実装クラス
+ * 減算を行う
+ */
 public class SubCalc implements Calculator {
 
 	@Override
 	public Integer calc(Integer x, Integer y) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return x - y;
 	}
 
 }
